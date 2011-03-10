95d17b6278bc7d5232d177120f6f7bbe5a37d846
Charles Feduke <charles.feduke@gmail.com>
2011-03-09 20:48:40 -0500

    refactored away branching in controllers and added yml for flash messages

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class NavigationTest < ActiveSupport::IntegrationCase
-  test "truth" do
-    assert_kind_of Dummy::Application, Rails.application
+  test "sets flash messages automatically" do
+    visit "/users"
+    
+    click_link "New User"
+    fill_in "Name", :with => "John Doe"
+    click_button "Create User"
+    
+    assert has_content?("User was successfully created."), "Expected to show flash message on create"
+    
+    click_link "Edit"
+    fill_in "Name", :with => "Doe, John"
+    click_button "Update User"
+    
+    assert has_content?("User was successfully updated."), "Expected to show flash message on update"
+    
+    click_link "Back"
+    click_link "Destroy"
+    
+    assert has_content?("User was successfully destroyed."), "Expected to show flash message on destroy"
   end
 end
